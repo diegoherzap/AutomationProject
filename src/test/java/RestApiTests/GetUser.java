9daa7c6d1ed1cce597af6813d48d0f913e87ab41
Diego Zapién <diego.herzap@gmail.com>
2018-07-07 19:16:35 -0500

    Added GET http requests to get single user by id and firstName

--- src/test/java/RestApiTests/GetUser.java
+++ src/test/java/RestApiTests/GetUser.java
@@ -3,16 +3,28 @@ package RestApiTests;
 import org.testng.annotations.Test;
 
 import io.restassured.RestAssured;
+import static com.jayway.restassured.RestAssured.*;
 import io.restassured.response.Response;
 import junit.framework.Assert;
 
 public class GetUser {
-  @Test
-  public void getUserReturns200WithExpectedName() {
-	String baseUrl = "http://localhost:3000";
-	Response resp = RestAssured.get(baseUrl+"/users/{id}",1);
-	int code = resp.getStatusCode();
-	System.out.println("Status code is: "+ code);
-	Assert.assertEquals(200, code);
+  
+	private static String baseUrl = "http://localhost:3000";
+	
+	@Test
+	public void getUserReturns200WithExpectedName() {
+		Response resp = RestAssured.get(baseUrl+"/users/{id}",1);
+		int code = resp.getStatusCode();
+		System.out.println("Status code is: "+ code);
+		Assert.assertEquals(200, code);
+  }
+  
+	@Test
+	public void getUser2Returns200WithExpectedName() {
+		given().
+		when().
+			get(baseUrl+"/users/?firstName=John").
+		then()
+			.statusCode(200);
   }
 }
